2fd9e2c262d65dce8d1e5bd646bb59f12161034a
Adam Lindberg <hello@alind.io>
2013-05-05 15:18:05 +0200

    Find correct files

--- lib/run_test.rb
+++ lib/run_test.rb
@@ -1,7 +1,27 @@
 module RunTest
 
   def self.command(args)
-    args.first
+    expression = args.first
+    if File.exist? expression
+      opts = expression
+    elsif expression =~ /^[A-Z]/
+      clazz = expression
+      clazz = "#{clazz}Test" unless clazz.end_with? "Test"
+      opts = `grep -R "^class #{clazz}" -l test`
+    else
+      expression = expression.dup
+      expression.gsub!(/_/, "[ _]") unless expression =~ /^".*"$/
+      files = `grep -R "#{expression}" -l test`.split("\n")
+      file = files.sort_by!{ |f| File.basename(f).size }.first
+      opts = "#{file}"
+    end
+    die("could not find tests for #{expression}") if opts.nil? || opts == ""
+    "ruby -Itest #{opts}"
+  end
+
+  def self.die(message, code = 1)
+    puts "error: #{message}"
+    exit(code)
   end
 
 end
--- spec/lib/run_test_spec.rb
+++ spec/lib/run_test_spec.rb
@@ -2,7 +2,65 @@ require 'spec_helper'
 require 'run_test'
 
 describe RunTest do
-  it "understands files" do
-    expect(RunTest.command ["my_file.rb"]).to eq "my_file.rb"
+
+  it "finds files" do
+    file = "my_file.rb"
+    File.should_receive(:exist?).with(file).and_return(true)
+    expect(RunTest.command [file]).to eq "ruby -Itest #{file}"
   end
+
+  it "finds classes" do
+    ["SomeTest", "OtherTest", "TheClass"].each do |clazz|
+      test_clazz = clazz.dup
+      test_clazz = "#{clazz}Test" unless clazz.end_with? "Test"
+      clazz_file = test_clazz.gsub(/(?<!^)[A-Z]/) { |c| "_#{c.downcase}" }
+      clazz_file = clazz_file.downcase # Remove first capital letter
+      path = "path/to/#{clazz_file}.rb"
+      expect_grep("^class #{test_clazz}", path)
+      expect(command [clazz]).to eq "ruby -Itest #{path}"
+    end
+  end
+
+  it "finds underscore names" do
+    ["test_case", "test_another"].each do |test_case|
+      path = "path/to/file_for_#{test_case}.rb"
+      expect_grep(test_case.gsub(/_/, "[ _]"), path)
+      expect(command [test_case]).to eq "ruby -Itest #{path}"
+    end
+  end
+
+  it "finds string names" do
+    ['"test spaces"', '"test_more"'].each do |test_case|
+      file = "file_for_" << test_case.slice(1..test_case.size-2).gsub(/ /, "_")
+      path = "path/to/#{file}.rb"
+      expect_grep(test_case, path)
+      expect(command [test_case]).to eq "ruby -Itest #{path}"
+    end
+  end
+
+  it "selects the shortest file when several matches" do
+    test_case = '"my test"'
+    paths = ["path/to/file_one.rb", "path/to/file_2.rb"]
+    expect_grep(test_case, paths.join("\n"))
+    expect(command [test_case]).to eq "ruby -Itest #{paths[1]}"
+  end
+
+  it "exits when no tests are found" do
+    RunTest.stub!(:puts)
+    test_case = '"non_existing_test"'
+    expect_grep(test_case, "")
+    expect{ command [test_case] }.to raise_error
+  end
+end
+
+def expect_grep(expression, return_value)
+  RunTest.should_receive(:`)
+         .with("grep -R \"#{expression}\" -l test")
+         .and_return(return_value)
+end
+
+# Mimic ARGV as a frozen list of strings
+def command(args)
+  args.map { |arg| arg.freeze }
+  RunTest.command(args.freeze)
 end
